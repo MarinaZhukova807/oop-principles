bbcf6345d1b9561861b9967ebeef6bf0a1b7fb6f
Марина Жукова <marinazhukova807@gmail.com>
2022-06-13 07:05:16 +0300

    first commit

---
+++ src/main/java/ru/netology/domain/Main.java
@@ -0,0 +1,20 @@
+package ru.netology.domain;
+
+import ru.netology.manager.FormDate;
+import ru.netology.manager.Post;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 4444444";
+        post.patronomyc = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+    }
+}
---
+++ src/main/java/ru/netology/manager/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.manager;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; //паспорт
+    public String patronomyc; //отчетсво
+    public String phone; //номер телефона
+    public String surname; //фамилия
+    public Boolean subscription; //подписка: да/нет
+    public FormDate birthday; //дата рождения
+}
